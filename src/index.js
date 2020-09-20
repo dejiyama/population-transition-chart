65dd4a1991ff24e28c8764ef4430afcb8741d94e
dejiyama <deziyama0404@gmail.com>
2020-09-20 11:39:43 +0900

    unify files name with camelCase

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import thunk from 'redux-thunk';
 import './index.css';
 import reducer from './reducers';
 
-import EventsIndex from './components/events_index';
+import EventsIndex from './components/eventsIndex';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer, applyMiddleware(thunk))
